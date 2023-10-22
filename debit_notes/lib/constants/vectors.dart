88bdc1285bbd2a6d5f2333530888effdd5149414
bakicpp <bakiyumak05@gmail.com>
2023-10-23 00:46:21 +0200

    code format

--- debit_notes/lib/constants/vectors.dart
+++ debit_notes/lib/constants/vectors.dart
@@ -6,4 +6,16 @@ class Vectors {
     height: 200,
     image: AssetImage('images/empty_state.png'),
   );
+
+  static const Image loginImage = Image(
+    width: 250,
+    height: 250,
+    image: AssetImage('images/login_screen_vector.png'),
+  );
+
+  static const Image signUpImage = Image(
+    width: 250,
+    height: 250,
+    image: AssetImage('images/sign_up_screen_vector.jpg'),
+  );
 }
